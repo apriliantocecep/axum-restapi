bf369aa8352bf9308777b6f12ea247de9621a531
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-03-26 11:55:58 +0700

    feat: add tracing and graceful shutdown

--- src/api/server.rs
+++ src/api/server.rs
@@ -9,7 +9,10 @@ use crate::api::{
     }
 };
 use std::str::FromStr;
-use tokio::net::TcpListener;
+use tokio::{
+    net::TcpListener,
+    signal::{self, unix::{self, SignalKind}}
+};
 
 pub async fn start() {
     let router = Router::new()
@@ -17,9 +20,35 @@ pub async fn start() {
 
     let addr = SocketAddr::from_str(&format!("{}:{}", "127.0.0.1", "8000")).unwrap();
     let listener = TcpListener::bind(&addr).await.unwrap();
+    tracing::info!("listening on {}", addr);
 
     axum::serve(listener, router)
+        .with_graceful_shutdown(shutdown_signal())
         .await
-        .unwrap()
+        .unwrap();
+
+    tracing::info!("server shutdown successfully");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c().await.expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        unix::signal(SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {}
+    }
 }
 
